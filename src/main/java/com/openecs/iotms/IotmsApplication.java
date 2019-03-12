ba278c2258e84a17a68f630b93088293c5a4c5c7
Issac Lee <issacl@MacBook-Pro-IssacL.local>
2019-03-12 10:58:14 -0700

    Initial commit

---
+++ src/main/java/com/openecs/iotms/IotmsApplication.java
@@ -0,0 +1,13 @@
+package com.openecs.iotms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotmsApplication.class, args);
+	}
+
+}
