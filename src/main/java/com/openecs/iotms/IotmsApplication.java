157e3dec3b3b347d9038f8185ff61799432e46ff
Issac Lee <issacl@MacBook-Pro-IssacL.local>
2019-04-22 23:16:01 -0700

    Added H2 schema and data

--- src/main/java/com/openecs/iotms/IotmsApplication.java
+++ src/main/java/com/openecs/iotms/IotmsApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -21,6 +23,24 @@ public class IotmsApplication {
 
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		// Execute OS command
+		// https://www.baeldung.com/run-shell-command-in-java
+		boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+		try {
+			String process;
+			// getRuntime: Returns the runtime object associated with the current Java application.
+			// exec: Executes the specified string command in a separate process.
+			Process p = Runtime.getRuntime().exec("ps -few");
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while ((process = input.readLine()) != null) {
+				System.out.println(process); // <-- Print all Process here line
+												// by line
+			}
+			input.close();
+		} catch (Exception err) {
+			err.printStackTrace();
+		}	
 		
 		return args -> {
 			/*
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,20 @@ create table student
    passport_number varchar(255) not null,
    primary key(id)
 );
+
+create table symbol_list (
+  symbol char(16) not null,
+  company_name varchar(255) not null
+) 
+as select * from CSVRead('data/nasdaq-listed-symbols_csv.csv');
+
+create table nasdaq_list (
+  symbol char(16) not null,
+  company_name varchar(255) not null,
+  security_name varchar(255),
+  market_category char(1),
+  test_issue char(1),
+  financial_status char(1),
+  round_lot_size decimal(5,2)
+) 
+as select * from CSVRead('data/nasdaq-listed_csv.csv');
